a6970d9e3e14db2bacb4ac6b525ab44265adfb09
jenjungjj <124737965+jenjungjj@users.noreply.github.com>
2023-05-04 21:14:08 -0400

    Add files via upload
    added frontend utils for api setup

---
+++ spotify-art-generator/src/utils/spotify.js
@@ -0,0 +1,23 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+export const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+export const redirectUri = process.env.REACT_APP_SPOTIFY_REDIRECT_URI;
+
+export const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "playlist-read-private",
+  "playlist-read-collaborative",
+];
+
+export const getLoginUrl = () => {
+  return `${authEndpoint}?client_id=${clientId}&redirect_uri=${encodeURIComponent(
+    redirectUri
+  )}&scope=${encodeURIComponent(scopes.join(" "))}&response_type=token`;
+};
+
+export const getTokenFromUrl = () => {
+  const urlParams = new URLSearchParams(window.location.hash.substring(1));
+  const access_token = urlParams.get("access_token");
+  return { access_token };
+};
